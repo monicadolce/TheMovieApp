f64e4df08c1447dcc0371900fdbf8ca65a37cba4
Monica <monicadolcemascolo@gmail.com>
2023-03-16 13:04:53 -0400

    created api endpoints in script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2b31555dd752e1d1c4b2406e4e4783b3&page=1'
+
+const IMG_PATH = 'https://images.tmdb.org/t/p/w1280/'
+
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=2b31555dd752e1d1c4b2406e4e4783b3&query="'
+
